fd25dd7baa50f683c701ec4ee3348c9bb64ab66f
Alexandr <natsunonekoaaron@gmail.com>
2021-05-05 21:48:59 +0500

    third commit

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
-"use strict";
+'use strict';
 
 let incr = 10,
-    decr = 10;
+  decr = 10;
 
 //incr++;
 //decr--;
 
 console.log(incr++, decr--);
-console.log (5%2); 
-console.log (2*4 === 8);
+console.log(5 % 2);
+console.log(2 * 4 === 8);
 
 const isChecked = true,
-      isClose = true;
+  isClose = true;
 
-      console.log (isChecked && isClose);
+console.log(isChecked && isClose);
