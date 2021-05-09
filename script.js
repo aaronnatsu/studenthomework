0f0f83a7d310e0ab164b03a03022e7cc5ab22b97
Alexandr <natsunonekoaaron@gmail.com>
2021-05-09 22:06:22 +0500

    added mdbpractice3

--- script.js
+++ script.js
@@ -1,29 +1,71 @@
-/* let admin;
-let myName;
+/*
+let userName = prompt('What is your name?', 'Denis');
+let userCity = prompt('Where are you from?', 'Saratov');
+let userAge = +prompt('How old are you?', '18');
 
-myName = 'John';
-admin = myName;
+let login = (userAge < 18) ? `We are so sorry, ${userName}` :
+    (userAge >= 18) ? `Welcome, ${userName} from ${userCity}!` : 'Error';
 
-console.log(admin);
-
-const planet = 'Earth';
-const currentUsername = '';
-
-console.log(planet);
-
-let age = +prompt('How old are you?', '26');
-let ageDays = (age * +365);
-let youSure = confirm(`Are you ${ageDays} days old?`);
-
-if (youSure == true) {
-    alert('Oh, wow');
-} else {
-    alert('Ehh');
-}
+alert(login);
 */
 
-let userName = prompt ('What is your name?', 'Valentin');
-let userCity = prompt ('Where do you live?', 'Nursultan');
-let checkUserData = confirm (`So, youre ${userName} from ${userCity}, yeah?`);
 
-alert (`Oh, welcome ${userName}!!!`);
+/*
+let question = prompt ('What if official name of Javascript?', '');
+
+let answer = (question == 'ECMAScript') ? 'Yes!' : 'Err.. No.';
+alert ( answer );
+*/
+
+
+/*
+let userNumber = +prompt ('Write random number', '');
+let checkNumber = (userNumber > 0) ? '1' :
+                  (userNumber < 0) ? '-1' : 
+                  (userNumber == 0) ? '0' : 'Error';
+
+alert ( checkNumber );
+*/
+
+
+/*
+let login = prompt ('Who are you', '');
+let loginMessage = (login == 'user') ? 'hello' :
+                (login == 'boss') ? 'dobriden' :
+                (login == '') ? 'enter login' : '';
+
+alert ( loginMessage );
+*/
+
+
+/*
+let age = +prompt('How old are you', '');
+
+if (!(age >= 6 && age <= 90)) {
+    alert('You can play lego');
+};
+*/
+
+
+/*
+const userLogin = prompt('Enter your login here', 'Admin');
+
+function checkLogin() {
+    let correctLogin = 'Admin';
+    let loginResult = (userLogin == correctLogin) ? checkPass() :
+        (userLogin == '' || userLogin == null) ? alert('Cancelled') :
+        alert('Can not find user');
+}
+
+function checkPass() {
+    let masterPass = 123456;
+    let userPass = +prompt('Please, enter password', '');
+    let passResult = (userPass == masterPass) ? 'Access granted' :
+        (userPass == '' || userPass == null) ? 'Cancelled' : 'Access denied';
+    alert(passResult);
+}
+
+checkLogin();
+*/
+
+
